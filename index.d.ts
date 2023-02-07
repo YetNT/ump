99f7859e2e926cc113e241bf76a65de5ebfbd834
Yetity <https://Yetity@github.com>
2023-02-07 19:56:43 +0200

    doing some typescript

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 declare function DinRatio(x: number, y:number, z:number): void
 declare function PropRatio(x: number, y:number, z:number, which?: boolean): void
+declare function PrimeFactorize(num: number, concFactors?: boolean): void
+declare function ConDist(): void
 
-export = {DinRatio, PropRatio}
+export = {DinRatio, PropRatio, PrimeFactorize, ConvDist}

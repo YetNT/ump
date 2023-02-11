5490fb1bda912ae39bff5bd4ac54ae988ffee345
Yetity <hlonipoole692@gmail.com>
2023-02-11 21:15:40 +0200

    conversion table - to micrometre. and defaulted current conversions

--- libmath/conversionTable.js
+++ libmath/conversionTable.js
@@ -59,6 +59,72 @@ const toNM = (n, ext) => {
 			var ans = n / 1609000000000;
 			return ans
 			break;
+		default:
+			return "how did you get this"
 	}
 }
-module.exports = {toNM}
+
+const toMicroM = (n, ext) => {
+	switch(ext){
+		case "nm":
+		case "nanometre":
+		case "nanometer":
+			var ans = n * 1000;
+			return ans
+			break;
+		case "mm":
+		case "milimeter":
+		case "milimetre":
+			var ans = n / 1000;
+			return ans
+			break;
+		case "cm":
+		case "centimeter":
+		case "centimeter":
+			var ans = n / 10000
+			return ans
+			break;
+		case "m":
+		case "meter":
+		case "metre":
+			var ans = n / 1000000;
+			return ans
+			break;
+		case "km":
+		case "kilometer":
+		case "kilometre":
+			var ans = n / 1000000000;
+			return ans
+			break;
+		case "nmi":
+		case "nautical-mile":
+			var ans = n * 1852;
+			return ans
+			break;
+		case "″": // inch but it's the real symbol
+		case '"': //
+		case "inch":
+			var ans = n / 25400;
+			return ans
+			break;
+		case "ft":
+		case "foot":
+			var ans = n / 304800;
+			return ans
+			break;
+		case "yd":
+		case "yard":
+			var ans = n / 914400;
+			return ans
+			break;
+		case "mi":
+		case "mile":
+			var ans = n / 1609000;
+			return ans
+			break;
+		default:
+			return "how did you get this"
+
+	}
+}
+module.exports = {toNM, toMicroM}

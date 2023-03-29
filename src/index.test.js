24666f2b1b07b1437b24c3bdf3c39ec203ec0bd4
Yetity <hlonipoole692@gmail.com>
2023-03-29 09:19:46 +0200

    Moved from jest to Mocha because yes.

--- src/index.test.js
+++ src/index.test.js
@@ -1,25 +1,70 @@
-const i = require('./index.js');
+const i = require('./index.js')
+const expect = require('expect.js')
 
-test('Divides 500 into the ratio of 3:2, = [300, 200]', () => {
-	expect(i.DinRatio(500, 3, 2)).toStrictEqual([300, 200])
+describe('Divide In Ratio', function() {
+	describe('#DinRatio()', function() {
+        it('should expose a function', function() {
+            expect(i.DinRatio).to.be.a('function')
+        })
+
+		it('should Divide 500 into the ratio of 3:2, returning [300, 200]', function() {
+            var ans = i.DinRatio(500, 3, 2)
+			expect(ans).to.eql([300, 200])
+		})
+	})
 })
 
-test('Gives the prime factors of 5000, which should be [2, 2, 2, 5, 5, 5, 5]', () => {
-	expect(i.PrimeFactorize(5000)).toStrictEqual([2, 2, 2, 5, 5, 5, 5])
+describe('Prime Factorization', function () {
+    describe('#PrimeFactorize', function() {
+        it('should expose a function', function() {
+            expect(i.PrimeFactorize).to.be.a('function')
+        })
+
+        it('should give the prime factors of 5000, returning [2, 2, 2, 5, 5, 5, 5]', function() {
+            var ans = i.PrimeFactorize(5000)
+            expect(ans).to.eql([2, 2, 2, 5, 5, 5, 5])
+        })
+    })
 })
 
-test('Divides x by z, and multiplies that by y. (x=2. y=4, 6=z) (which? = true) = 12', () => {
-	expect(i.PropRatio(2, 4, 6, true)).toStrictEqual(12)
+describe('Propertional Ratios', function () {
+    describe('#PropRatio', function() {
+        it('should expose a function', function() {
+            expect(i.PropRatio).to.be.a('function')
+        })
+
+        it('should divide x by z, multiplying that by y (x=2, y=4, 6=z, which?=true) = 12', function() {
+            var ans = i.PropRatio(2, 4, 6, true)
+            expect(ans).to.eql(12)
+        })
+    })
 })
 
-test('Converts 1 nautical-mile to nanometre, = 1852000000000', () => {
-	expect(i.ConvUnit("dist", 1, "nautical-mile", "nanometre")).toStrictEqual(1852000000000)
-})
+describe('Conversion of Units', function() {
+    describe('#ConvUnit', function() {
+        it('should expose a function', function() {
+            expect(i.ConvUnit).to.be.a('function')
+        })
 
-test('Converts 1 square-centimre to square-inch, = 0.155', () => {
-	expect(i.ConvUnit("area", 1, "square-centimetre", "square-inch")).toStrictEqual(0.15500031000062)
-})
+        describe('Distance', function() {
+            it('should convert 1 nautical-mile to nanometre = 1852000000000', function() {
+                var ans = i.ConvUnit('dist', 1, 'nautical-mile', 'nanometre')
+                expect(ans).to.eql(1852000000000)
+            })
+        })
 
-test("Converts 1 cubic metre to cubic centimetre, = 1000000", () => {
-	expect(i.ConvUnit("vol", 1, "m3", "cm3")).toStrictEqual(1000000)
+        describe('Area', function() {
+            it('should convert 1 square-centimetre to square-inch = 0.15500031000062', function() {
+                var ans = i.ConvUnit('area', 1,'square-centimetre','square-inch')
+                expect(ans).to.eql(0.15500031000062)
+            })
+        })
+
+        describe('Volume', function() {
+            it('should conevrt 1 cubic metre to cubic centimetre = 1000000', function() {
+                var ans = i.ConvUnit('vol', 1, 'cubic-metre', 'cubic-centimetre')
+                expect(ans).to.eql(1000000)
+            })
+        })
+    })
 })
